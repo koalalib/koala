5b4b35bfa9c2490a6aba7d6476e4c253afe6e3b3
Giaro <giaro@kaims.eti.pg.gda.pl>
2014-03-03 15:56:24 +0100

    Signed-off-by: Giaro <giaro@kaims.eti.pg.gda.pl>

--- container/simple.hpp
+++ container/simple.hpp
@@ -109,42 +109,4 @@ template< class T > template< class InputIterator >
 }
 
 
-template< class T, class Comp >
-	PriQueueInterface< T *,Comp >::PriQueueInterface( T* bufor, T* end, int max, Comp acomp ):
-		buf( bufor ), maxsize( max ), siz( end - bufor ), comp( acomp )
-{
-	koalaAssert( siz >= 0 && siz <= maxsize,ContExcWrongArg );
-	std::make_heap( buf,end,comp );
-}
 
-template< class T, class Comp > void PriQueueInterface< T *,Comp >::push( const T &val )
-{
-	koalaAssert( siz < maxsize,ContExcFull );
-	buf[siz++] = val;
-	std::push_heap( buf,buf + siz,comp);
-}
-
-template< class T, class Comp > void PriQueueInterface< T *,Comp >::pop()
-{
-	koalaAssert( siz,ContExcOutpass );
-	std::pop_heap( buf,buf + siz,comp );
-	siz--;
-}
-
-template< class T, class Comp > T PriQueueInterface< T *,Comp >::top() const
-{
-	koalaAssert( siz,ContExcOutpass );
-	return buf[0];
-}
-
-template< class T, class Comp > template< class InputIterator >
-	void PriQueueInterface< T *,Comp >::assign( InputIterator first, InputIterator last )
-{
-	clear();
-	for( siz = 0; first != last; first++,siz++ )
-	{
-		buf[siz] = (*first);
-		koalaAssert( siz < maxsize,ContExcFull );
-	}
-	std::make_heap( buf,buf + siz,comp );
-}
