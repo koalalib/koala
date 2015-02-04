011bece8299c52e80906068d6d37317b004ca180
obszar <pawob@eti.pg.gda.pl>
2015-02-04 23:45:07 +0100

    doxy 2-SAT na bazie komentarzy autora

--- koala/algorithm/sat2cnf.h
+++ koala/algorithm/sat2cnf.h
@@ -8,61 +8,60 @@
 
 namespace Koala
 {
-	//NEW
-	/*
-	Class contains methods for solving 2-SAT problem according to the algorithm described in:
-	"A linear-time algorithm for testing the truth of certain quantified boolean formulas", B. Aspvall, M.F. Plass, R.E. Tarjan,
-	Information Processing Letters, Volume 8, Issue 3, Pages 121–123, 1979.
-	*/
-
+	/** \brief 2-SAT solution.
+	 *
+	 *  Class contains methods for solving 2-SAT problem according to the algorithm described in:\n
+	 *  "A linear-time algorithm for testing the truth of certain quantified boolean formulas", B. Aspvall, M.F. Plass, R.E. Tarjan,
+	 *  Information Processing Letters, Volume 8, Issue 3, Pages 121–123, 1979. */
 	template<class DefaultStructs> class Sat2CNFPar
 	{
 	public:
 
-		/*
-		Type Literal represents an literal that can appear in a clauses in 2-SAT problem:
-		- int  - nonnegative variable number,
-		- bool  - true means that the variable is in plain form (without negation),
-		- false means that the variable is appears with negation.
-		*/
+		/** \brief Literal type.
+		 *
+		 * Type Literal represents an literal that can appear in a clauses in 2-SAT problem:
+		 * - int  - nonnegative variable number,
+		 * - bool  - true means that the variable is in plain form (without negation),
+		 * - false means that the variable is appears with negation. */
 		typedef std::pair<int, bool> Literal;
-		/*
-		Type Class represents a clause (i.e., alternative of two literals) that can appear in a clauses in 2-SAT problem
-		*/
+		
+		/** \brief Clause type.
+		 *
+		 * Type Class represents a clause (i.e., alternative of two literals) that can appear in a clauses in 2-SAT problem */
 		typedef std::pair<Literal, Literal> Clause;
 
-		/*
-		The method returns the number of distinct variables.
-		Iter should be an iterator over a collection of Clause objects.
-		IterOut should be an iterator over a collection of int variables.
-		- Iter begin - points to the beginning of a collection of clauses,
-		- Iter end - points to the past-the-end element for a collection of clauses.
-		- IterOut out - output iterator for storing numbers of variables in increasing order.
-		*/
+		/** \brief Get variables.
+		 *
+		 * The method returns the number of distinct variables.
+		 * Iter should be an iterator over a collection of Clause objects.
+		 * IterOut should be an iterator over a collection of int variables.
+		 * \param begin - points to the beginning of a collection of clauses,
+		 * \param end - points to the past-the-end element for a collection of clauses.
+		 * \param out - output iterator for storing numbers of variables in increasing order.*/
 		template<class Iter, class IterOut>
 		static int vars(Iter begin, Iter end, IterOut out);
 
-		/*
-		The method returns true if 2-SAT problem given as input has a solution, otherwise it returns false.
-		Iter should be an iterator over a collection of Clause objects.
-		IterOut should be an iterator over a collection of bool variables.
-		- Iter begin - points to the beginning of a collection of clauses,
-		- Iter end - points to the past-the-end element for a collection of clauses.
-		- IterOut out - output iterator for storing bool variables (values of variables in increasing order)
-		that represent the solution if one exists.
-		*/
+		/** \brief Solve 2-SAT.
+		 *
+		 * The method returns true if 2-SAT problem given as input has a solution, otherwise it returns false.
+		 * Iter should be an iterator over a collection of Clause objects.
+		 * IterOut should be an iterator over a collection of bool variables.
+		 * \param begin - points to the beginning of a collection of clauses,
+		 * \param end - points to the past-the-end element for a collection of clauses.
+		 * \param out - output iterator for storing bool variables (values of variables in increasing order)
+		 *  that represent the solution if one exists.
+		 */
 		template<class Iter, class IterOut>
 		static bool solve(Iter begin, Iter end, IterOut out);
 
-		/*
-		The method returns the value of a 2-SAT problem given as input for the realization of variables given as input by begin2 and end2 pointers.
-		Iter should be an iterator over a collection of Clause objects.
-		Iter2 should be an iterator over a collection of bool variables.
-		- Iter begin - points to the beginning of a collection of clauses,
-		- Iter end - points to the past-the-end element for a collection of clauses.
-		- Iter2 begin2 - points to the beginning of a collection of bool values,
-		- Iter2 end2 - points to the past-the-end element for a collection of bool values.
-		*/
+		/** \brief Evaluate.
+		 * The method returns the value of a 2-SAT problem given as input for the realization of variables given as input by begin2 and end2 pointers.
+		 * Iter should be an iterator over a collection of Clause objects.
+		 * Iter2 should be an iterator over a collection of bool variables.
+		 * \param begin - points to the beginning of a collection of clauses,
+		 * \param end - points to the past-the-end element for a collection of clauses.
+		 * \param begin2 - points to the beginning of a collection of bool values,
+		 * \param end2 - points to the past-the-end element for a collection of bool values.*/
 		template<class Iter, class Iter2>
 		static bool eval(Iter begin, Iter end, Iter2 begin2, Iter2 end2);
 
